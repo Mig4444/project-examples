e3047e1cd0b83104db646953f2fb87dec819b7a6
Boris Kurtis Bignumb-bi-Kumb <boris-kurtis.bignumb-bi-kumb@protonmail.com>
2021-11-11 19:26:00 +0100

    Add some example source files.

---
+++ cmake-application/include/hello-app.hpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include <string>
+
+const std::string hello_app_print()
+{
+	std::string str = "test";
+	return str;
+}
---
+++ cmake-application/src/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdio>
+#include "../include/hello-app.h"
+
+int main(int argc, char *argv[])
+{
+	std::cout << hello_app_print() << std::endl;
+	return EXIT_SUCCESS;
+}
