8408f23387ca48c06cfc862d91bcfcd396991a96
Boris Kurtis Bignumb-bi-Kumb <boris-kurtis.bignumb-bi-kumb@protonmail.com>
2021-11-11 19:29:24 +0100

    Remove hello.cpp

--- cmake-application/src/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstdio>
-#include "../include/hello-app.h"
-
-int main(int argc, char *argv[])
-{
-	std::cout << hello_app_print() << std::endl;
-	return EXIT_SUCCESS;
-}
